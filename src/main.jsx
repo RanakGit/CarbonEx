3b44573f871dd30f6660e23e7a8fbdd020c9692f
Ranak Halder <ranakhalder1@gmail.com>
2026-06-10 13:08:00 +0530

    Create React root mount point in src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CarbonEx from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CarbonEx />
+  </React.StrictMode>
+)
